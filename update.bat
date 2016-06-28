3af1a799af948184a2886e74f453fa1ef6bbd060
Eduardo ZF <eduardozf1@gmail.com>
2016-06-27 21:33:20 -0300

    Update

--- update.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-color 02
-@echo  ########################
-@echo  ###    ATUALIZANDO   ###
-@echo  ########################
-git init
-git pull https://github.com/edu12378/tok.git
-timeout /t 3 /nobreak > NUL
-echo. 2>"C:\Users\Eduardo\Documents\Visual Studio 2015\Projects\WindowsFormsApplication1\WindowsFormsApplication1\obj\Debug\update.txt"
